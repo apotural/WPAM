9baf2e3adc1f4327e7a92dfb3de23df7d06aa0d5
alicjap <p.ala@vp.pl>
2020-04-29 21:24:01 +0200

    Add most basic login to facebook

--- App/app/src/main/java/com/poturalakonieczka/hellyeeeah/FacebookLoginFragment.kt
+++ App/app/src/main/java/com/poturalakonieczka/hellyeeeah/FacebookLoginFragment.kt
@@ -1,164 +1,196 @@
-package com.poturalakonieczka.hellyeeeah
-
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.google.firebase.auth.FirebaseAuth
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-
-class FacebookLoginFragment : Fragment(), View.OnClickListener{
-    // [START declare_auth]
-    private lateinit var auth: FirebaseAuth
-    // [END declare_auth]
-
-    //private lateinit var binding: ActivityFacebookBinding
-    //private lateinit var callbackManager: CallbackManager
-    companion object {
-        fun newInstance() = FacebookLoginFragment()
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.facebook_login_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-    }
-
-
-    override fun onClick(v: View?) {
-        TODO("Not yet implemented")
-    }
-    /*
-
-
-
-    public override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityFacebookBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setProgressBar(binding.progressBar)
-
-        binding.buttonFacebookSignout.setOnClickListener(this)
-
-        // [START initialize_auth]
-        // Initialize Firebase Auth
-        auth = Firebase.auth
-        // [END initialize_auth]
-
-        // [START initialize_fblogin]
-        // Initialize Facebook Login button
-        callbackManager = CallbackManager.Factory.create()
-
-        binding.buttonFacebookLogin.setReadPermissions("email", "public_profile")
-        binding.buttonFacebookLogin.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
-            override fun onSuccess(loginResult: LoginResult) {
-                Log.d(TAG, "facebook:onSuccess:$loginResult")
-                handleFacebookAccessToken(loginResult.accessToken)
-            }
-
-            override fun onCancel() {
-                Log.d(TAG, "facebook:onCancel")
-                // [START_EXCLUDE]
-                updateUI(null)
-                // [END_EXCLUDE]
-            }
-
-            override fun onError(error: FacebookException) {
-                Log.d(TAG, "facebook:onError", error)
-                // [START_EXCLUDE]
-                updateUI(null)
-                // [END_EXCLUDE]
-            }
-        })
-        // [END initialize_fblogin]
-    }
-
-    // [START on_start_check_user]
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        updateUI(currentUser)
-    }
-    // [END on_start_check_user]
-
-    // [START on_activity_result]
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Pass the activity result back to the Facebook SDK
-        callbackManager.onActivityResult(requestCode, resultCode, data)
-    }
-    // [END on_activity_result]
-
-    // [START auth_with_facebook]
-    private fun handleFacebookAccessToken(token: AccessToken) {
-        Log.d(TAG, "handleFacebookAccessToken:$token")
-        // [START_EXCLUDE silent]
-        showProgressBar()
-        // [END_EXCLUDE]
-
-        val credential = FacebookAuthProvider.getCredential(token.token)
-        auth.signInWithCredential(credential)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithCredential:success")
-                        val user = auth.currentUser
-                        updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithCredential:failure", task.exception)
-                        Toast.makeText(baseContext, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show()
-                        updateUI(null)
-                    }
-
-                    // [START_EXCLUDE]
-                    hideProgressBar()
-                    // [END_EXCLUDE]
-                }
-    }
-    // [END auth_with_facebook]
-
-    fun signOut() {
-        auth.signOut()
-        LoginManager.getInstance().logOut()
-
-        updateUI(null)
-    }
-
-    private fun updateUI(user: FirebaseUser?) {
-        hideProgressBar()
-        if (user != null) {
-            binding.status.text = getString(R.string.facebook_status_fmt, user.displayName)
-            binding.detail.text = getString(R.string.firebase_status_fmt, user.uid)
-
-            binding.buttonFacebookLogin.visibility = View.GONE
-            binding.buttonFacebookSignout.visibility = View.VISIBLE
-        } else {
-            binding.status.setText(R.string.signed_out)
-            binding.detail.text = null
-
-            binding.buttonFacebookLogin.visibility = View.VISIBLE
-            binding.buttonFacebookSignout.visibility = View.GONE
-        }
-    }
-
-    override fun onClick(v: View) {
-        if (v.id == R.id.buttonFacebookSignout) {
-            signOut()
-        }
-    }
-
-    companion object {
-        private const val TAG = "FacebookLogin"
-    }
-     */
-}
+//package com.poturalakonieczka.hellyeeeah
+//
+//import android.content.Intent
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.fragment.app.Fragment
+//import com.facebook.*
+//import com.facebook.FacebookSdk.getApplicationContext
+//import com.facebook.login.LoginManager
+//import com.facebook.login.LoginResult
+//import com.google.firebase.auth.FacebookAuthProvider
+//import com.google.firebase.auth.FirebaseAuth
+//import com.google.firebase.auth.FirebaseUser
+//import kotlinx.android.synthetic.main.facebook_login_fragment.*
+//import java.util.*
+//
+//
+//class FacebookLoginFragment : Fragment() {
+//
+//    lateinit var auth: FirebaseAuth
+//    lateinit var user: FirebaseUser
+//    var callbackManager: CallbackManager = CallbackManager.Factory.create();
+//
+//    companion object {
+//        fun newInstance() = FacebookLoginFragment()
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        return inflater.inflate(R.layout.facebook_login_fragment, container, false)
+//    }
+//
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//        auth = FirebaseAuth.getInstance()
+//        user = auth.currentUser!!
+//        if(user != null) {
+//            FacebookSdk.sdkInitialize(getApplicationContext())
+//            login_button.setReadPermissions(Arrays.asList("email"))
+//        }else {
+//
+//        }
+//    }
+//
+//    fun buttonClickFbLogin(v: View) {
+//        LoginManager.getInstance().registerCallback(callbackManager,
+//            object : FacebookCallback<LoginResult?> {
+//                override fun onSuccess(loginResult: LoginResult?) {
+//                    // App code
+//                }
+//
+//                override fun onCancel() {
+//                    // App code
+//                }
+//
+//                override fun onError(exception: FacebookException) {
+//                    // App code
+//                }
+//            })
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        if(callbackManager != null) {
+//            callbackManager?.onActivityResult(requestCode, resultCode, data)
+//        }
+//        super.onActivityResult(requestCode, resultCode, data)
+//    }
+//}
+//    /*
+//    public override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityFacebookBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        setProgressBar(binding.progressBar)
+//
+//        binding.buttonFacebookSignout.setOnClickListener(this)
+//
+//        // [START initialize_auth]
+//        // Initialize Firebase Auth
+//        auth = Firebase.auth
+//        // [END initialize_auth]
+//
+//        // [START initialize_fblogin]
+//        // Initialize Facebook Login button
+//        callbackManager = CallbackManager.Factory.create()
+//
+//        binding.buttonFacebookLogin.setReadPermissions("email", "public_profile")
+//        binding.buttonFacebookLogin.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+//            override fun onSuccess(loginResult: LoginResult) {
+//                Log.d(TAG, "facebook:onSuccess:$loginResult")
+//                handleFacebookAccessToken(loginResult.accessToken)
+//            }
+//
+//            override fun onCancel() {
+//                Log.d(TAG, "facebook:onCancel")
+//                // [START_EXCLUDE]
+//                updateUI(null)
+//                // [END_EXCLUDE]
+//            }
+//
+//            override fun onError(error: FacebookException) {
+//                Log.d(TAG, "facebook:onError", error)
+//                // [START_EXCLUDE]
+//                updateUI(null)
+//                // [END_EXCLUDE]
+//            }
+//        })
+//        // [END initialize_fblogin]
+//    }
+//
+//    // [START on_start_check_user]
+//    public override fun onStart() {
+//        super.onStart()
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        val currentUser = auth.currentUser
+//        updateUI(currentUser)
+//    }
+//    // [END on_start_check_user]
+//
+//    // [START on_activity_result]
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        // Pass the activity result back to the Facebook SDK
+//        callbackManager.onActivityResult(requestCode, resultCode, data)
+//    }
+//    // [END on_activity_result]
+//
+//    // [START auth_with_facebook]
+//    private fun handleFacebookAccessToken(token: AccessToken) {
+//        Log.d(TAG, "handleFacebookAccessToken:$token")
+//        // [START_EXCLUDE silent]
+//        showProgressBar()
+//        // [END_EXCLUDE]
+//
+//        val credential = FacebookAuthProvider.getCredential(token.token)
+//        auth.signInWithCredential(credential)
+//                .addOnCompleteListener(this) { task ->
+//                    if (task.isSuccessful) {
+//                        // Sign in success, update UI with the signed-in user's information
+//                        Log.d(TAG, "signInWithCredential:success")
+//                        val user = auth.currentUser
+//                        updateUI(user)
+//                    } else {
+//                        // If sign in fails, display a message to the user.
+//                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+//                        Toast.makeText(baseContext, "Authentication failed.",
+//                                Toast.LENGTH_SHORT).show()
+//                        updateUI(null)
+//                    }
+//
+//                    // [START_EXCLUDE]
+//                    hideProgressBar()
+//                    // [END_EXCLUDE]
+//                }
+//    }
+//    // [END auth_with_facebook]
+//
+//    fun signOut() {
+//        auth.signOut()
+//        LoginManager.getInstance().logOut()
+//
+//        updateUI(null)
+//    }
+//
+//    private fun updateUI(user: FirebaseUser?) {
+//        hideProgressBar()
+//        if (user != null) {
+//            binding.status.text = getString(R.string.facebook_status_fmt, user.displayName)
+//            binding.detail.text = getString(R.string.firebase_status_fmt, user.uid)
+//
+//            binding.buttonFacebookLogin.visibility = View.GONE
+//            binding.buttonFacebookSignout.visibility = View.VISIBLE
+//        } else {
+//            binding.status.setText(R.string.signed_out)
+//            binding.detail.text = null
+//
+//            binding.buttonFacebookLogin.visibility = View.VISIBLE
+//            binding.buttonFacebookSignout.visibility = View.GONE
+//        }
+//    }
+//
+//    override fun onClick(v: View) {
+//        if (v.id == R.id.buttonFacebookSignout) {
+//            signOut()
+//        }
+//    }
+//
+//    companion object {
+//        private const val TAG = "FacebookLogin"
+//    }
+//     */
--- App/app/src/main/java/com/poturalakonieczka/hellyeeeah/MainActivity.kt
+++ App/app/src/main/java/com/poturalakonieczka/hellyeeeah/MainActivity.kt
@@ -1,18 +1,103 @@
 package com.poturalakonieczka.hellyeeeah
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuth.AuthStateListener
+import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 class MainActivity : AppCompatActivity() {
+    private var callbackManager: CallbackManager? = null
+    private var fbAuth: FirebaseAuth? = null
+    private var authListener: AuthStateListener? = null
+    private val TAG = "FacebookAuth"
 
+    private fun exchangeAccessToken(token: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        fbAuth!!.signInWithCredential(credential)
+            .addOnCompleteListener(this
+            ) { task ->
+                if (!task.isSuccessful) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        callbackManager = CallbackManager.Factory.create();
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, FacebookLoginFragment.newInstance())
-                .commitNow()
+        loginButton.setReadPermissions("email", "public_profile");
+
+        loginButton.registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                    Log.d(TAG, "onSuccess: $loginResult")
+                    exchangeAccessToken(loginResult.accessToken)
+                }
+
+                override fun onCancel() {
+                    Log.d(TAG, "onCancel: User cancelled sign-in")
+                }
+
+                override fun onError(error: FacebookException) {
+                    Log.d(TAG, "onError: $error")
+                }
+            })
+
+        fbAuth = FirebaseAuth.getInstance()
+
+        authListener = AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                emailText.text = user.email
+                statusText.text = "Signed In"
+            } else {
+                emailText.text = ""
+                statusText.text = "Signed Out"
+
+            }
         }
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, FacebookLoginFragment.newInstance())
+//                .commitNow()
+//        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        fbAuth!!.addAuthStateListener(authListener!!)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (authListener != null) {
+            fbAuth!!.removeAuthStateListener(authListener!!)
+        }
+    }
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager!!.onActivityResult(requestCode, resultCode, data)
     }
 }
