c4c8d3d18323558b75c0fc8445e66b29c9abba47
alicjap <p.ala@vp.pl>
2020-05-13 20:12:39 +0200

    Add slow calendar

--- App/app/src/main/java/com/poturalakonieczka/hellyeeeah/CalendarFragment.kt
+++ App/app/src/main/java/com/poturalakonieczka/hellyeeeah/CalendarFragment.kt
@@ -5,16 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.CalendarMode
 import kotlinx.android.synthetic.main.user_calendar_fragment.*
 import java.util.*
 
 
 class CalendarFragment: Fragment(){
-    val calendar: Calendar = Calendar.getInstance()
+    val TAG = "My-deb calendar"
+
     companion object {
         fun newInstance() = CalendarFragment()
     }
-
     private lateinit var viewModel: ModelView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -22,13 +24,17 @@ class CalendarFragment: Fragment(){
         return inflater.inflate(R.layout.user_calendar_fragment, container, false)
     }
 
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        calendarView.state().edit()
+            .setMinimumDate(CalendarDay.from(2016, 4, 3))
+            .setMaximumDate(CalendarDay.from(2016, 5, 12))
+            .setCalendarDisplayMode(CalendarMode.MONTHS)
+            .commit();
 
-        val oneMonthsAhead = Calendar.getInstance()
-        oneMonthsAhead.add(Calendar.MONTH, 1)
-
-        calendarView.minDate = Date().time - 1000L*60L*60L*24L*365L
-        calendarView.maxDate = Date().time + 1000L*60L*60L*24L*30L
+        //calendarView.minDate = Date().time - 1000L*60L*60L*24L*365L
+        //calendarView.maxDate = Date().time + 1000L*60L*60L*24L*30L
     }
+
 }
